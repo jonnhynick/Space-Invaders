347c657c655e89b348baf279fd1724af54275e2c
jonnhynick <nickolai.mtz@gmail.com>
2017-08-25 13:08:53 -0700

    Add files via upload

---
+++ SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public static SoundManager Instance = null;
+
+    public AudioClip alienBuzz1;
+    public AudioClip alienBuzz2;
+    public AudioClip alienDies;
+    public AudioClip bulletFire;
+    public AudioClip shipExplosion;
+    private AudioSource soundEffectAudio;
+
+    // Use this for initialization
+    void Start () {
+		if(Instance == null)
+        {
+            Instance = this;
+        }else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource mySource = GetComponent<AudioSource>();
+        soundEffectAudio = mySource;
+	}
+	
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+}
---
+++ SpaceShip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour {
+    public float speed = 30;    //represents the speed of set object
+
+    public GameObject theBullet;
+    void FixedUpdate()  //needed when rigid body components are used in order to move the object
+    {
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontalMove, 0) * speed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(theBullet, transform.position, Quaternion.identity);
+
+            SoundManager.Instance.PlayOneShot(SoundManager.Instance.bulletFire);
+        }
+	}
+}
